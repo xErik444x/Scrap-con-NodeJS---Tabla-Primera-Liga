80e10a15556d26fe58fa869f5822063da8aeca39
Erik Schwerdt <erikschwerdt18@gmail.com>
2022-01-09 16:59:01 -0300

    posicionesController was updated with a new database management

--- controller/posicionesController.js
+++ controller/posicionesController.js
@@ -1,21 +1,37 @@
 //controller clear database and save the scrap info from scrap_results to db
 
 const {scrapResults} = require("../scrapper/scrap_results");
-const tabla_posiciones = require('../models/Tabla_posiciones')
+const tabla_posiciones = require('../models/Tabla_posiciones');
+const { off } = require("../models/Tabla_posiciones");
 
 module.exports.Tabla_posiciones_saveToBd = async ()=>{
     try {
         const table = await scrapResults();
         if(table.length<26 ||table[0]==="Error"){
-            console.warn("Error, no data in table. - posicionesController")
-            return("Error, no data in table. - posicionesController")
+            console.warn("Error, no data in table. - posicionesController");
+            return("Error, no data in table. - posicionesController");
         }
-        //console.log("actualizando datos en bd")
-        //clear database and upload new data
-        tabla_posiciones.deleteMany({}).then(res =>{
+        const data = await tabla_posiciones.find({});
+
+        if(data.length<1){
+            //if no data, upload all scrap data
             tabla_posiciones.insertMany(table)
-        })
-        
+
+        }else{
+            //compare scrap data and database data
+            //if scraping is different from database, update them
+            table.forEach((e,i) => {
+                if(e.team_name_large != data[i].team_name_large){
+                    tabla_posiciones.findByIdAndUpdate(data[i].id,e, function(err){
+                        if(err){
+                            console.warn(err)
+                        }
+                        //updated successfully
+                    })
+                }
+            });
+        }
+
     } catch (error) {
         console.error(error)
     }
